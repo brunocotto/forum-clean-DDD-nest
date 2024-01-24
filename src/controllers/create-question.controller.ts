39600ed93d6a01d34df5dea54e1a154b7372647f
Bruno Otto <bruno.ottoo@gmail.com>
2024-01-24 18:06:00 -0300

    tokenPayload to UserPayload

--- src/controllers/create-question.controller.ts
+++ src/controllers/create-question.controller.ts
@@ -1,13 +1,27 @@
-import { Controller, Post, UseGuards } from '@nestjs/common'
+import { Body, Controller, Post, UseGuards, UsePipes } from '@nestjs/common'
 import { CurrentUser } from 'src/auth/current-user-decorator'
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard'
-import { TokenPayload } from 'src/auth/jwt.strategy'
+import { UserPayload } from 'src/auth/jwt.strategy'
+import { ZodValidationPipe } from 'src/pipes/zod-validation.pipe'
+import { z } from 'zod'
+
+const createQuestionBodySchema = z.object({
+  title: z.string(),
+  content: z.string(),
+})
+
+type CreateQuestionBodySchema = z.infer<typeof createQuestionBodySchema>
 
 @UseGuards(JwtAuthGuard)
 @Controller('/questions')
 export class CreateQuestionController {
   @Post()
-  async handle(@CurrentUser() user: TokenPayload) {
+  @UsePipes(new ZodValidationPipe(createQuestionBodySchema))
+  async handle(
+    @Body()
+    body: CreateQuestionBodySchema,
+    @CurrentUser() user: UserPayload,
+  ) {
     console.log(user)
   }
 }
