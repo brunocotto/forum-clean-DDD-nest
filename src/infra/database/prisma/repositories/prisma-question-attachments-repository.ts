d94c193862fea5c84a97e74ef6475a79d2abdf0f
brunocotto <bruno.ottoo@gmail.com>
2024-02-17 08:05:07 -0300

    feat: conversa entre camadas utiliazando mappers

--- src/infra/database/prisma/repositories/prisma-question-attachments-repository.ts
+++ src/infra/database/prisma/repositories/prisma-question-attachments-repository.ts
@@ -1,11 +1,14 @@
 import { QuestionAttachmentRepository } from '@/domain/forum/application/repositories/question-attachments-repository'
 import { QuestionAttachment } from '@/domain/forum/enterprise/entities/question-attachment'
 import { Injectable } from '@nestjs/common'
+import { PrismaService } from '../prisma.service'
 
 @Injectable()
 export class PrismaQuestionAttachementsRepository
   implements QuestionAttachmentRepository
 {
+  constructor(private prisma: PrismaService) {}
+
   findManyByQuestionId(questionId: string): Promise<QuestionAttachment[]> {
     throw new Error('Method not implemented.')
   }
