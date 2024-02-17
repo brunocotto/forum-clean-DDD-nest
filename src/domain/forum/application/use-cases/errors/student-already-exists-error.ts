6a334cc20b5a0f7fa232a0855eed36c7d7affff4
brunocotto <bruno.ottoo@gmail.com>
2024-02-17 10:10:07 -0300

    feat: casos de uso de autenticação e cadastro de students

---
+++ src/domain/forum/application/use-cases/errors/student-already-exists-error.ts
@@ -0,0 +1,6 @@
+import { UseCaseError } from '@/core/errors/use-case-error'
+export class StudentAlreadyExistsError extends Error implements UseCaseError {
+  constructor() {
+    super('Student with same e-mail adress already exists.')
+  }
+}
