d5f13287ddd234008bc73cc4cb9382be1d1cc779
brunocotto <bruno.ottoo@gmail.com>
2024-02-17 20:31:40 -0300

    feat: criando mappers do prisma

---
+++ prisma/migrations/20240217232822_enter_with_question_and_answer_in_attachment/migration.sql
@@ -0,0 +1,9 @@
+-- AlterTable
+ALTER TABLE "attachments" ADD COLUMN     "answer_id" TEXT,
+ADD COLUMN     "question_id" TEXT;
+
+-- AddForeignKey
+ALTER TABLE "attachments" ADD CONSTRAINT "attachments_question_id_fkey" FOREIGN KEY ("question_id") REFERENCES "questions"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "attachments" ADD CONSTRAINT "attachments_answer_id_fkey" FOREIGN KEY ("answer_id") REFERENCES "answers"("id") ON DELETE SET NULL ON UPDATE CASCADE;
